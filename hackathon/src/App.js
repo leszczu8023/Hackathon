4c6650fbdbdede7f447be142bc2a99a5866cb030
Michał Lichuszewski <Mikus1pa12ry12@interia.pl>
2019-04-13 12:25:23 +0200

    commit 12:25

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,10 +1,31 @@
+<<<<<<< HEAD
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./component/header.js";
+import Menu from "./component/menu.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
+=======
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./component/header.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
         <header className="App-header">Hello</header>
       </div>
     );
@@ -12,3 +33,4 @@ class App extends Component {
 }
 
 export default App;
+>>>>>>> 3506299f03840484cf38121ef52c119808cb3856
---
+++ hackathon/src/component/header.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return null;
+  }
+}
+
+export default Header;
+=======
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return <div>header</div>;
+  }
+}
+
+export default Header;
+>>>>>>> 3506299f03840484cf38121ef52c119808cb3856
