76cd812a3ba7a87edba679dd1bcef915c23a2a44
Michał Lichuszewski <Mikus1pa12ry12@interia.pl>
2019-04-13 18:59:48 +0200

    commit 19:00

--- hackathon/src/component/sites/defaultcontent.js
+++ hackathon/src/component/sites/defaultcontent.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React,{Component} from "react";
 
-const defaultcontent = () => {
-    return (
+class defaultcontent extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            login:''
+        };
+        this.Change = this.Change.bind(this);
+        this.PostLog = this.PostLog.bind(this);
+    }
+    Change(e) {
+    console.log(e.target.value);
+    this.setState({
+        login: e.target.value
+    });
+    }
+    PostLog(e){
+    e.preventDefault();
+    console.log(this.state);
+    };
+    render(){
+        return (
         <section>
-
-                <label>Login</label><input type="text" name="login"/>
-                <label>Password</label><input type="password" name="password"/>
+            <form id="PostLog" onSubmit={this.PostLog}>
+                <label>Login</label><input type="text" name="login" value={this.state.login} onChange={this.Change}/>
+                <label>Password</label><input type="password" name="password" value={this.state.password} onChange={this.Change}/>
                 <input type="submit"/>
-
+            </form>
         </section>
-    )
+    )}
 }
 export default defaultcontent;
