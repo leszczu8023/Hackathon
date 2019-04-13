5d2b486b1390d16e9d8e4dbc3f4521e3dc63179f
Michał Lichuszewski <Mikus1pa12ry12@interia.pl>
2019-04-13 19:21:20 +0200

    Sat Apr 13 19:21:19 DST 2019

---
+++ hackathon/src/component/sites/defaultcontent.js
@@ -0,0 +1,50 @@
+import React,{Component} from "react";
+import apiAuthenticate from "../../config.js"
+
+class defaultcontent extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            login:'',
+            password:'',
+            isLoad:''
+        };
+        this.Change = this.Change.bind(this);
+        this.ChangePassword = this.ChangePassword.bind(this);
+        this.PostLog = this.PostLog.bind(this);
+    }
+    Change(e) {
+    this.setState({
+        login: e.target.value
+    });
+    }
+    ChangePassword(e) {
+    this.setState({
+        password: e.target.value
+    });
+    }
+    PostLog(e){
+    e.preventDefault();
+    fetch(apiAuthenticate)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoad: result.type
+          });
+        }
+      );
+    console.log(this.state.isLoad)
+    };
+    render(){
+        return (
+        <section>
+            <form id="PostLog" onSubmit={this.PostLog}>
+                <label>Login</label><input type="text" name="login" value={this.state.login} onChange={this.Change}/>
+                <label>Password</label><input type="password" name="password" value={this.state.password} onChange={this.ChangePassword}/>
+                <input type="submit"/>
+            </form>
+        </section>
+    )}
+}
+export default defaultcontent;
